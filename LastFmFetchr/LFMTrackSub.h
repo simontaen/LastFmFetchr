066a8f67d81e18d23ca145eeb90e589272ed9a74
Simon Tännler <simontea@edgeguard.ch>
2013-11-18 00:33:52 +0100

    LFMTrack committing to save it

---
+++ LastFmFetchr/LFMTrackSub.h
@@ -0,0 +1,22 @@
+//
+//  LFMTrack.h
+//  LastFmFetchr
+//
+//  Created by Simon Tännler on 17/11/13.
+//  Copyright (c) 2013 edgeguard. All rights reserved.
+//
+
+#import "LFMData.h"
+#import "LFMArtist.h"
+
+@interface LFMTrackSub : LFMData
+
+@property (nonatomic, strong, readonly) NSNumber *rank;
+@property (nonatomic, strong, readonly) LFMArtist *artist;
+@property (nonatomic, strong, readonly) NSNumber *duration;
+@property (nonatomic, strong, readonly) NSString *musicBrianzId;
+@property (nonatomic, strong, readonly) NSString *name;
+@property (nonatomic, readonly) BOOL isTrackStreamable;
+@property (nonatomic, strong, readonly) NSURL *lfmPage;
+
+@end
